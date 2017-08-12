430dd2ec0ce2643519e9af6efdf64d136475a68a
tilyd <tilyd@DESKTOP-N7QGF6N>
2017-08-12 18:10:28 +0900

    hi

--- sickSa/src/main/java/sickSa/dao/QueueAwaiterDao.java
+++ sickSa/src/main/java/sickSa/dao/QueueAwaiterDao.java
@@ -1,8 +1,28 @@
 package sickSa.dao;
 
+import java.util.List;
 
-public class QueueAwaiterDao {
-	/* Variable */
-	/* Constructor */
-	/* Method */
+import sickSa.domain.QueueAwaiter;
+
+/*
+**********************************
+* Table Name: 대기자정보 *
+**********************************
+CREATE TABLE QUEUE_AWAITERS(
+		QAWT_ID                       		NUMBER(6)		 NULL ,
+		QAWT_CONTACT                  		VARCHAR2(16)		 NULL 
+);
+
+COMMENT ON TABLE QUEUE_AWAITERS is '대기자정보';
+COMMENT ON COLUMN QUEUE_AWAITERS.QAWT_ID is '대기자번호';
+COMMENT ON COLUMN QUEUE_AWAITERS.QAWT_CONTACT is '대기자연락처';
+
+
+ALTER TABLE QUEUE_AWAITERS ADD CONSTRAINT IDX_QUEUE_AWAITERS_PK PRIMARY KEY (QAWT_ID);
+*/
+public interface QueueAwaiterDao {
+	public List<QueueAwaiter> selectAllQueueAwaiters();
+	public void insertQueueAwaiter(QueueAwaiter queueAwaiter);
+	public void updateQueueAwaiter(QueueAwaiter queueAwaiter);
+	public void deleteQueueAwaiter(int qawt_id);
 }
--- sickSa/src/main/java/sickSa/dao/QueueLogDao.java
+++ sickSa/src/main/java/sickSa/dao/QueueLogDao.java
@@ -1,5 +1,32 @@
 package sickSa.dao;
 
-public class QueueLogDao {
+import java.util.List;
 
+import sickSa.domain.QueueAwaiter;
+import sickSa.domain.QueueLog;
+
+/*
+**********************************
+* Table Name: 대기로그 *
+********************************** 
+CREATE TABLE QUEUE_LOGS(
+		QLOG_ID                       		NUMBER(6)		 NULL ,
+		QLOG_RECORD                   		DATE		 DEFAULT SYSDATE		 NOT NULL,
+		QAWT_ID                       		NUMBER(6)		 NULL 
+);
+
+COMMENT ON TABLE QUEUE_LOGS is '대기로그';
+COMMENT ON COLUMN QUEUE_LOGS.QLOG_ID is '대기번호';
+COMMENT ON COLUMN QUEUE_LOGS.QLOG_RECORD is '대기발생일시';
+COMMENT ON COLUMN QUEUE_LOGS.QAWT_ID is '대기자번호';
+
+
+ALTER TABLE QUEUE_LOGS ADD CONSTRAINT IDX_QUEUE_LOGS_PK PRIMARY KEY (QLOG_ID);
+ALTER TABLE QUEUE_LOGS ADD CONSTRAINT IDX_QUEUE_LOGS_FK0 FOREIGN KEY (QAWT_ID) REFERENCES QUEUE_AWAITERS (QAWT_ID);
+*/
+public interface QueueLogDao {
+	public List<QueueLog> selectAllQueueLogs();
+	public void insertQueueLog(QueueLog queueLog);
+	public void updateQueueLog(QueueLog queueLog);
+	public void deleteQueueLog(int qlog_id);
 }
