3b3faad26d653f0867c6985997e5af433b06c2aa
tilyd <tilyd@192.168.219.101>
2017-08-13 00:44:53 +0900

    edit ProductCategories

--- sickSa/src/main/java/sickSa/dao/ProductCategoriesDao.java
+++ sickSa/src/main/java/sickSa/dao/ProductCategoriesDao.java
@@ -1,5 +1,26 @@
 package sickSa.dao;
 
-public class ProductCategoriesDao {
+import java.util.List;
 
+import sickSa.domain.ProductCategories;
+
+/*
+**********************************
+* Table Name: 상품카테고리 *
+/**********************************
+CREATE TABLE PRODUCT_CATEGORIES(
+		PDCT_ID                       		NUMBER(6)		 NULL ,
+		PDCT_NAME                     		VARCHAR2(20)		 NOT NULL
+);
+
+COMMENT ON TABLE PRODUCT_CATEGORIES is '상품카테고리';
+COMMENT ON COLUMN PRODUCT_CATEGORIES.PDCT_ID is '상품카테고리번호';
+COMMENT ON COLUMN PRODUCT_CATEGORIES.PDCT_NAME is '상품카테고리명';
+
+
+ALTER TABLE PRODUCT_CATEGORIES ADD CONSTRAINT IDX_PRODUCT_CATEGORIES_PK PRIMARY KEY (PDCT_ID);
+*/
+public interface ProductCategoriesDao {
+	public List<ProductCategories> selectAllProductCategories();
+	public ProductCategories selectProductCategoriesById(Integer pdct_id);
 }
