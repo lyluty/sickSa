1130ef24f8169ebb914effa07283248b346cc29c
Woo <Woo@Woo-PC>
2017-08-15 13:15:32 +0900

    ingredient test

--- sickSa/src/main/java/sickSa/controller/IngredientsController.java
+++ sickSa/src/main/java/sickSa/controller/IngredientsController.java
@@ -1,9 +1,24 @@
 package sickSa.controller;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import sickSa.dao.IngredientsDao;
 
 @Controller
 public class IngredientsController {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	 
+	@RequestMapping("/test/mybatis/{userId}")
+	public String testMybatis(@PathVariable("userId") String userId) {
+	    return "";
+	}
 	
 	
 }
