00b1cc8263816e5610f59489536c669711cfea80
tilyd <tilyd@DESKTOP-N7QGF6N>
2017-08-16 15:53:54 +0900

    add Service

--- sickSa/src/main/java/sickSa/service/QueueService.java
+++ sickSa/src/main/java/sickSa/service/QueueService.java
@@ -1,16 +1,15 @@
 package sickSa.service;
 
+/*
+ * 대기자 등록
+ * 대기 취소
+ * 대기자 호출
+ * 입실
+ * 퇴실
+ */
 public interface QueueService {
 	public void queueUp(String qlog_contact);
 	public void enter(Integer qlog_id);
 	public void leave(Integer qlog_id);
 	public void callAwaiter(Integer qlog_id);
-	
-	/*
-	 * 대기자 리스트 조회
-	 * 특정 대기자 찾기(번호, 연락처)
-	 * 번호 가장 빠른사람 찾기
-	 * 취소(대기번호 삭제)
-	 * 대기번호 발급
-	 */
 }
---
+++ sickSa/src/main/java/sickSa/service/SalesService.java
@@ -0,0 +1,10 @@
+package sickSa.service;
+
+/*
+ * 전체 판매내역 조회 (최근순)
+ * 주문번호로 주문정보 조회
+ * ...
+ */
+public interface SalesService {
+
+}
