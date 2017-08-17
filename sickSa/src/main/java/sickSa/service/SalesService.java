67443526f475aee3d33dde24002c072a2176cfe7
LynJ <LynJ@DESKTOP-65GOFTL>
2017-08-17 17:56:43 +0900

    Sales Service

--- sickSa/src/main/java/sickSa/service/SalesService.java
+++ sickSa/src/main/java/sickSa/service/SalesService.java
@@ -1,6 +1,10 @@
 package sickSa.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import sickSa.domain.Order;
 
 /*
  * 전체 판매내역 조회 (최근순)
@@ -10,5 +14,7 @@ import org.springframework.stereotype.Service;
 @Service("SalesService")
 public interface SalesService {
 
+		public List<Order> getOrdList();
+		public  Order getOrdListByNo(Integer ord_no);
 	
 }
