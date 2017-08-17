487f803eaccf3e29d8685d6f1021f330d89a4f40
STU <STU@ITWILL>
2017-08-17 18:11:52 +0900

    insert done

--- sickSa/src/main/java/sickSa/controller/IngredientsController.java
+++ sickSa/src/main/java/sickSa/controller/IngredientsController.java
@@ -3,6 +3,7 @@ package sickSa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,32 +16,27 @@ public class IngredientsController {
 	
 	@Autowired
 	private IngredientsMapper ingredientsMapper;
-	@Autowired
-	private IngredientCategoriesMapper ingredientCategoriesMapper; 
 	 
-	@RequestMapping("/test/ingredients/{ing_id}")
-	public String testIngredients(Model model,@PathVariable("ing_id") Ingredients ingredients) {
-//		model.addAttribute(
-//				"ingredientList", ingredientsMapper.ingredientList());
-//		model.addAttribute(
-//				"ingredient", ingredientsMapper.selectIngredientById(ing_id));
-		model.addAttribute("insertIngredient", ingredientsMapper.insertIngredient(ingredients));
-		
-	    return "test/testIngredients";
+	@RequestMapping("/selectIngredients/{ing_id}")
+	public String selectIngredientsTest(Model model,@PathVariable("ing_id") Integer ing_id) {
+		model.addAttribute(
+				"ingredientList", ingredientsMapper.ingredientList());
+		model.addAttribute(
+				"ingredient", ingredientsMapper.selectIngredientById(ing_id));
+	    return "ingredientsSelect";
 	}
 	
-	@RequestMapping("/test/ingredientCategories/{igct_id}")
-	public String testIngredientCategories(
-			Model model,@PathVariable("igct_id") Integer igct_id){
-		if (igct_id == null)
-			System.out.println("null");
-		else
-			System.out.println(igct_id);
-		model.addAttribute(
-				"ingredientCategoryList", ingredientCategoriesMapper.ingredientCategoriesList());
-		model.addAttribute(
-				"ingredientCategory", ingredientCategoriesMapper.selectIngredientCategoriesById(igct_id));
-		return "test/testIngredientCategories";
+	@RequestMapping("/insertFormIngredients")
+	public String insertFormIngredientsTest(){
+		
+		return "ingredientsInsertForm";
+	}
+	
+	@RequestMapping("/insertActionIngredients")
+	public String insertActionIngredientsTest(
+			Model model,@ModelAttribute("ingredient") Ingredients ingredients){
+		ingredientsMapper.insertIngredient(ingredients);
+		return "forward:/selectIngredients/"+ingredients.getIng_id();
 	}
 	
 	@Autowired
