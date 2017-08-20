c0aa3763fd5a78f2e6b55d72729840043d8f8a16
choiLeader <chs1109@naver.com>
2017-08-20 20:29:54 +0900

    static/*.html resource path modify

--- sickSa/src/main/webapp/WEB-INF/view/static/mail.php
+++ sickSa/src/main/webapp/WEB-INF/view/static/mail.php
@@ -14,6 +14,7 @@
 
     $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 	
+	
     $send = mail($to, $subject, $body, $headers);
 	
 ?>
