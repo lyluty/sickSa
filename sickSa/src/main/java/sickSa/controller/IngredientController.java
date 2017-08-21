9ff3a355c6ba1592aff58e1c1c7b69a47f25523a
BOKSOON <chs11099@naver.com>
2017-08-21 18:43:32 +0900

    jsp folder create

--- sickSa/src/main/java/sickSa/controller/IngredientController.java
+++ sickSa/src/main/java/sickSa/controller/IngredientController.java
@@ -14,13 +14,14 @@ public class IngredientController {
 	
 	@Autowired
 	private IngredientService ingredientService;
-	
+	/*
 	@RequestMapping("/igctList")
 	public String loadListIgct(Model model){
 		model.addAttribute(
 				"ingredientCategoriesList", ingredientService.loadListIgct());
 		return "ingredientCategoriesSelect";
 	}
+	*/
 	
 	@RequestMapping("/igctInsertForm")
 	public String addIgctForm(){
@@ -33,6 +34,14 @@ public class IngredientController {
 					IngredientCategories ingredientCategories){
 		model.addAttribute("insertIngredientCategories",ingredientService.addIgct(ingredientCategories));
 		return "forward:/IGCTSelect/"+ingredientCategories.getIgct_name();
+	}
+	
+	// Test
+	@RequestMapping("/igctList")
+	public String loadListIgct(Model model){
+		model.addAttribute(
+				"ingredientCategoriesList", ingredientService.loadListIgct());
+		return "/jsp/index";
 	}
 	
 }
