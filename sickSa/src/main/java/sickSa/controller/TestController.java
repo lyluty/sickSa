7cf133533df06096347ec9847ecc5506164fac64
tilyd <tilyd@DESKTOP-N7QGF6N>
2017-08-22 20:40:18 +0900

    test를 위한 컨트롤러/뷰

--- sickSa/src/main/java/sickSa/controller/TestController.java
+++ sickSa/src/main/java/sickSa/controller/TestController.java
@@ -2,6 +2,8 @@ package sickSa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -9,5 +11,22 @@ public class TestController {
 	@RequestMapping("testMain")
 	public String testMain() {
 		return "testIndex";
+	}
+	
+	@RequestMapping("testMain2")
+	public String testMain2() {
+		return "testIndex2";
+	}
+	
+	@RequestMapping("sales")
+	public String sales() {
+		return "sales";
+	}
+	
+	@RequestMapping("salesViewHtm")
+	public String salesViewHtml(@RequestParam String startDate, @RequestParam String endDate) {
+		System.out.println("start: " + startDate);
+		System.out.println("end: " + endDate);
+		return "salesView2";
 	}
 }
---
+++ sickSa/src/main/webapp/WEB-INF/view/testIndex2.jsp
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- head start -->
+<jsp:include page="include-head.jsp" flush="false" />
+<!-- head end -->
+</head>
+<body>
+  <div class="animsition">
+    <!-- header start -->
+    <jsp:include page="include-header.jsp" flush="false" />
+    <!-- header end -->
+    
+    <!-- 컨텐츠 영역 start -->
+    <div class="hero-container">
+    
+    </div>
+    <!-- 컨텐츠 영역 end -->
+
+    <!-- footer start -->
+    <jsp:include page="include-footer.jsp" flush="false" />
+    <!-- footer end -->
+  </div>
+  
+  <!-- tail start -->
+  <jsp:include page="include-tail.jsp" flush="false" />
+  <!-- tail end -->
+</body>
+</html>
