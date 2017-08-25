8d8dec37cf7c6b36e3b2aed9a6689326bc84a480
tilyd <tilyd@M-SurfaceBook>
2017-08-25 17:53:12 +0900

    에러 수정

--- sickSa/src/main/java/sickSa/service/OrderService.java
+++ sickSa/src/main/java/sickSa/service/OrderService.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import sickSa.dao.ProductCategoriesDao;
+import sickSa.dao.ProductDao;
 import sickSa.domain.Order;
 import sickSa.domain.OrderDetailVO;
+import sickSa.domain.OrderVO;
+import sickSa.domain.Product;
+import sickSa.domain.ProductCategories;
 import sickSa.mapper.OrderDao;
 
 @Service
@@ -15,35 +20,33 @@ public class OrderService {
 	@Autowired
 	private OrderDao orderDao;
 	@Autowired
-	private OrderDetailDao orderDetailDao;
-	
-	public List<OrderDetailVO> testOrder() {
-		return orderDetailDao.list();
-	}
-	
-	public List<Order> testOrder2() {
-		return orderDao.list();
-	}
+	private ProductDao productDao;
+	@Autowired
+	private ProductCategoriesDao productCategoriesDao;
 
 	/* 결제 완료 - 레코드 생성 */
-	public void add(Order order) {
+	public void add(OrderVO order) {
 		orderDao.insertOrder(order);
 		for (OrderDetailVO orderDetail : order.getOrderDetailList()) {
 			orderDao.insertOrderDetail(orderDetail);
 		}
 	}
 
-	public void set(Order order) {
+	public void set(OrderVO order) {
 		orderDao.updateOrder(order);
 	}
-	//
-	// /* find List by id */
-	// public Order getById(Integer ord_id) {
-	// return orderDao.selectOrder(ord_id);
-	// }
 
 	/* 전체목록 불러오기 */
 	public List<Order> orderList() {
 		return orderDao.selectOrderList();
+	}
+	
+	public List<ProductCategories> productCategoryList() {
+		return productCategoriesDao.selectAllProductCategories();
+	}
+	
+	public List<Product> productListByCategoryId(int categoryId) {
+//		return productDao.selectProductListByCategoryId(categoryId);
+		return null;
 	}
 }
