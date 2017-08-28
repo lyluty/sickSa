be0ff40ff2c766de2645cf943bbbbd20ae4baa83
WOO <wkungh93@gmail.com>
2017-08-28 22:00:51 +0900

    ㅇㅇ

--- sickSa/src/main/webapp/WEB-INF/view/ingredientListView.jsp
+++ sickSa/src/main/webapp/WEB-INF/view/ingredientListView.jsp
@@ -1,9 +1,25 @@
 <%@page import="org.springframework.ui.Model"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+
+
+<!-- 
+<style type="text/css">
+	div.panel > div.panel-body{
+		.toggleClass("selected");
+	}
+	
+</style>
+ -->
+ <style type="text/css">
+ 	
+ </style>
 <script src='include/js/ingredient.js'></script>
+
 <div id="ingListForm" class="panel">
 	<h3 class="content-title">Ingredient</h3>
+	
 	<!-- Ingredient List Slide  start -->
 	<ul class="sub-service-list">
 		<c:forEach var="ing" items="${ingredientList}">
@@ -12,19 +28,21 @@
 					<div class="panel-heading">
 						<div class="service-box">
 							<div class="service-num">${ing.ing_id}</div>
-							<div class="service-icon">
-								<i class="ion ion-android-color-palette"></i>
+							<div class="service-icon"><i class="ion ion-android-color-palette"></i>
 							</div>
 							<h6>${ing.ing_name}</h6>
 						</div>
 					</div>
 					<div class="panel-body">
-						<p>${ing.ing_stock}</p>
-						<p>${ing.ingredientDetails.igdt_price}</p>
+						<p>재고: ${ing.ing_stock}</br>
+						단가: ${ing.ingredientDetails.igdt_price}</p>
 					</div>
 				</div>
 			</li>
 		</c:forEach>
+		<li>
+			<img id="addImg" alt="plus" src="include/images/addButton.png" onclick="ing_insert_form();"/>
+		</li>
 	</ul>
 	<!-- Ingredient List Slide start -->
 </div>
--- sickSa/src/main/webapp/include/js/ingredient.js
+++ sickSa/src/main/webapp/include/js/ingredient.js
@@ -4,7 +4,7 @@
 
 /*****전역 초기화함수 로그인 체크******/
 function init_ui() {
-	alert('ㅇ?');
+	alert('관리자에오?');
 };
 
 /*****전역 로드시 실행******/
@@ -61,49 +61,27 @@ function ing_list_load(i){
 	});
 }
 
-function wrapWindowByMask(){
-    // 화면의 높이와 너비를 변수로 만듭니다.
-    var maskHeight = $(document).height();
-    var maskWidth = $(window).width();
-
-    // 마스크의 높이와 너비를 화면의 높이와 너비 변수로 설정합니다.
-    $('.mask').css({'width':maskWidth,'height':maskHeight});
-
-    // fade 애니메이션 : 1초 동안 검게 됐다가 80%의 불투명으로 변합니다.
-    $('.mask').fadeIn(1000);
-    $('.mask').fadeTo("slow",0.8);
-
-    // 레이어 팝업을 가운데로 띄우기 위해 화면의 높이와 너비의 가운데 값과 스크롤 값을 더하여 변수로 만듭니다.
-    var left = ( $(window).scrollLeft() + ( $(window).width() - $('.window').width()) / 2 );
-    var top = ( $(window).scrollTop() + ( $(window).height() - $('.window').height()) / 2 );
-
-    // css 스타일을 변경합니다.
-    $('.window').css({'left':left,'top':top, 'position':'absolute'});
-
-    // 레이어 팝업을 띄웁니다.
-    $('.window').show();
+function igct_insert_form(){
+	window.open(
+			'igctInsertForm','재료 항목 추가',
+			'width=700, height=500, left=0, top=0, toolbar=no, location=no, directories=no, status=no, menubar=no, resizable=no, scrollbars=no, copyhistory=no');
 }
 
-$(document).ready(function(){
-    // showMask를 클릭시 작동하며 검은 마스크 배경과 레이어 팝업을 띄웁니다.
-    $('img#addImg').click(function(e){
-        // preventDefault는 href의 링크 기본 행동을 막는 기능입니다.
-        e.preventDefault();
-        wrapWindowByMask();
-    });
+/*function igct_insert_action(){
+	document.getElementById("igctF").action = 'igctInsertAction';
+	document.getElementById("igctF").submit();
+}*/
 
-    // 닫기(close)를 눌렀을 때 작동합니다.
-    $('.window .close').click(function (e) {
-        e.preventDefault();
-        $('.mask, .window').hide();
-    });
+function ing_insert_form(){
+	window.open(
+			'ingInsertForm','재료 항목 추가',
+	'width=700, height=500, left=0, top=0, toolbar=no, location=no, directories=no, status=no, menubar=no, resizable=no, scrollbars=no, copyhistory=no');
+}
 
-    // 뒤 검은 마스크를 클릭시에도 모두 제거하도록 처리합니다.
-    $('.mask').click(function () {
-        $(this).hide();
-        $('.window').hide();
-    });
-});
+/*function ing_insert_action(){
+	document.f.action = 'ingInsertAction';
+	document.f.submit();
+}*/
 
 
 /**
@@ -111,6 +89,7 @@ $(document).ready(function(){
  */
 
 function ingredientCreate() {
+	document.igctAddForm.serialize();
 	document.f.action = '/insertActionIngredients';
 	document.f.submit();
 }
