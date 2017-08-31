1bd0b3e534aea660b11855762a90c8c8438abea5
tilyd <tilyd@M-SurfaceBook>
2017-08-31 18:31:41 +0900

    테스트용 컨트롤러임다

---
+++ sickSa/src/main/java/sickSa/controller/TestController.java
@@ -0,0 +1,19 @@
+package sickSa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sickSa.service.SalesService;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	SalesService salesService;
+	
+	@RequestMapping("test")
+	public String test() {
+		return "";
+	}
+}
