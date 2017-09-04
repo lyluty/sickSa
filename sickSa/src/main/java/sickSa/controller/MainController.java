c753c43dda2aa9399fa578b85dde01cd9cd4e9dd
tilyd <tilyd@M-SurfaceBook>
2017-09-04 18:39:07 +0900

    메인페이지에서 테이블 선택기능 추가

--- sickSa/src/main/java/sickSa/controller/MainController.java
+++ sickSa/src/main/java/sickSa/controller/MainController.java
@@ -1,13 +1,12 @@
 package sickSa.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import sickSa.service.OrderService;
 import sickSa.service.StoreService;
 
 /*
@@ -16,35 +15,37 @@ import sickSa.service.StoreService;
 @Controller
 public class MainController {
 
-	@Autowired
-	StoreService storeService;
+  @Autowired
+  StoreService storeService;
+  @Autowired
+  OrderService orderService;
 
-	@RequestMapping("mainIndex")
-	public String mainIndex(Model model) {
-		 /*
-		storeService.setRest();
-		storeService.setWaiting();
-		*/
-	
-		model.addAttribute("restCnt", storeService.getRest());
-		model.addAttribute("waitCnt", storeService.getWaiting());
-		
-		return "mainIndex";
-	}
-	
-	@RequestMapping("login")
-	public String login(){
-		
-		return "common/login";
-	}
-	@RequestMapping("matchPw")
-	public String matchPw(@RequestParam("pw_pin") String pw_pin){
-		if(storeService.checkPin(pw_pin)){			
-			return "sales/salesViewByTimePeriod";		//임시
-		}
-		return "common/login";
-	}
-	
-	
+  @RequestMapping("mainIndex")
+  public String mainIndex(Model model) {
+    /*
+     * storeService.setRest(); storeService.setWaiting();
+     */
+
+    // model.addAttribute("restCnt", storeService.getRest());
+    // model.addAttribute("waitCnt", storeService.getWaiting());
+    model.addAttribute("restCnt", orderService.getRestTableCount());
+    model.addAttribute("tableList", orderService.tableList());
+
+    return "mainIndex";
+  }
+
+  @RequestMapping("login")
+  public String login() {
+
+    return "common/login";
+  }
+
+  @RequestMapping("matchPw")
+  public String matchPw(@RequestParam("pw_pin") String pw_pin) {
+    if (storeService.checkPin(pw_pin)) {
+      return "sales/salesViewByTimePeriod"; // 임시
+    }
+    return "common/login";
+  }
+
 }
-
