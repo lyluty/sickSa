b93ebbc41e86dd3af787bc306a4d211643ad12f4
tilyd <tilyd@M-SurfaceBook>
2017-09-04 12:21:53 +0900

    으악

--- sickSa/src/main/java/sickSa/controller/OrderController.java
+++ sickSa/src/main/java/sickSa/controller/OrderController.java
@@ -15,102 +15,106 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import sickSa.domain.Order;
 import sickSa.domain.OrderDetail;
 import sickSa.domain.Product;
+import sickSa.service.IngredientService;
 import sickSa.service.OrderService;
 
 @Controller
 public class OrderController {
 
-	@Autowired
-	OrderService orderService;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    IngredientService ingredientService;
 
-	@RequestMapping("_productList")
-	public String _productList(@RequestParam("cateNo") int categoryId, Model model) {
-		List<Product> productList = orderService.productListByCategoryId(categoryId);
-		model.addAttribute("productList", productList);
-		return "order/_productList";
-	}
+    @RequestMapping("_productList")
+    public String _productList(@RequestParam("cateNo") int categoryId, Model model) {
+        List<Product> productList = orderService.productListByCategoryId(categoryId);
+        model.addAttribute("productList", productList);
+        return "order/_productList";
+    }
 
-	@RequestMapping("vmMain")
-	public String testProduct(Model model) {
-		model.addAttribute("productCategoryList", orderService.productCategoryList());
-		return "order/index";
-	}
+    @RequestMapping("vmMain")
+    public String testProduct(Model model) {
+        model.addAttribute("productCategoryList", orderService.productCategoryList());
+        return "order/index";
+    }
 
-	@RequestMapping("productListByCategoryId.ajax")
-	public String productListByCategoryIdAjax(Model model, @RequestParam int categoryId) {
-		List<Product> productList = orderService.productListByCategoryId(categoryId);
-		model.addAttribute("productList", productList);
-		return "order/productList";
-	}
+    @RequestMapping("productListByCategoryId.ajax")
+    public String productListByCategoryIdAjax(Model model, @RequestParam int categoryId) {
+        List<Product> productList = orderService.productListByCategoryId(categoryId);
+        model.addAttribute("productList", productList);
+        return "order/productList";
+    }
 
-	@RequestMapping("orderNow.ajax")
-	public String orderNowAjax(HttpSession session, @RequestParam int productId) {
-		session.removeAttribute("cart");
-		@SuppressWarnings("unchecked")
-		List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
-		Product product = orderService.getProduct(productId);
-		List<OrderDetail> cart = orderService.addToCart(sCart, product);
-		session.setAttribute("cart", cart);
-		return "order/choicePaymentMethod";
-	}
+    @RequestMapping("orderNow.ajax")
+    public String orderNowAjax(HttpSession session, @RequestParam int productId) {
+        session.removeAttribute("cart");
+        @SuppressWarnings("unchecked")
+        List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
+        Product product = orderService.getProduct(productId);
+        List<OrderDetail> cart = orderService.addToCart(sCart, product);
+        session.setAttribute("cart", cart);
+        return "order/choicePaymentMethod";
+    }
 
-	@RequestMapping("addToCart.ajax")
-	public String addToCart(HttpSession session, @RequestParam int productId) {
-		@SuppressWarnings("unchecked")
-		List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
-		Product product = orderService.getProduct(productId);
-		List<OrderDetail> cart = orderService.addToCart(sCart, product);
-		session.setAttribute("cart", cart);
-		return "order/cart";
-	}
+    @RequestMapping("addToCart.ajax")
+    public String addToCart(HttpSession session, @RequestParam int productId) {
+        @SuppressWarnings("unchecked")
+        List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
+        Product product = orderService.getProduct(productId);
+        List<OrderDetail> cart = orderService.addToCart(sCart, product);
+        session.setAttribute("cart", cart);
+        return "order/cart";
+    }
 
-	@RequestMapping("productDetail/{productId}")
-	public String productDetailAjax(Model model, @PathVariable int productId) {
-		Product product = orderService.getProduct(productId);
-		if (product == null)
-			return "order/index";
-		model.addAttribute("product", product);
-		return "order/productDetail";
-	}
+    @RequestMapping("productDetail/{productId}")
+    public String productDetailAjax(Model model, @PathVariable int productId) {
+        Product product = orderService.getProduct(productId);
+        if (product == null)
+            return "order/index";
+        model.addAttribute("product", product);
+        return "order/productDetail";
+    }
 
-	@RequestMapping("deleteProductFromCart")
-	@ResponseBody
-	public List<OrderDetail> deleteProductFromCart(HttpSession session, @RequestParam int pId) {
-		@SuppressWarnings("unchecked")
-		List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
-		for (OrderDetail orderDetail : sCart) {
-			if (orderDetail.getPdt_id() == pId) {
-				sCart.remove(orderDetail);
-				break;
-			}
-		}
-		return sCart.size() == 0 ? null : sCart;
-	}
+    @RequestMapping("deleteProductFromCart")
+    @ResponseBody
+    public List<OrderDetail> deleteProductFromCart(HttpSession session, @RequestParam int pId) {
+        @SuppressWarnings("unchecked")
+        List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
+        for (OrderDetail orderDetail : sCart) {
+            if (orderDetail.getPdt_id() == pId) {
+                sCart.remove(orderDetail);
+                break;
+            }
+        }
+        return sCart.size() == 0 ? null : sCart;
+    }
 
-	@RequestMapping("choicePaymentMethod.ajax")
-	public String choicePaymentMethodAjax() {
-		return "order/choicePaymentMethod";
-	}
+    @RequestMapping("choicePaymentMethod.ajax")
+    public String choicePaymentMethodAjax() {
+        return "order/choicePaymentMethod";
+    }
 
-	@RequestMapping("payNow")
-	public String payNowAjax(HttpSession session, Model model, @RequestParam String paymentMethod) {
-		@SuppressWarnings("unchecked")
-		List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
-		Order order = orderService.createOrder(sCart, paymentMethod);
-		model.addAttribute("order", order);
-		model.addAttribute("orderDetailList", sCart);
+    @RequestMapping("payNow")
+    public String payNowAjax(HttpSession session, Model model, @RequestParam String paymentMethod) {
+        @SuppressWarnings("unchecked")
+        List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
+        Order order = orderService.createOrder(sCart, paymentMethod);
+        model.addAttribute("order", order);
+        model.addAttribute("orderDetailList", sCart);
+        for (OrderDetail orderDetail : sCart) {
+            ingredientService.changeStock(orderDetail.getPdt_id());
+        }
+        session.removeAttribute("cart");
+        return "order/result";
+    }
 
-		session.removeAttribute("cart");
-
-		return "order/result";
-	}
-
-	@RequestMapping("setOrdtAmount")
-	@ResponseBody
-	public void setOrdtAmount(HttpSession session, @RequestParam int pId, @RequestParam int amount) {
-		@SuppressWarnings("unchecked")
-		List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
-		List<OrderDetail> cart = orderService.setOrdtAmount(sCart, pId, amount);
-		session.setAttribute("cart", cart);
-	}
+    @RequestMapping("setOrdtAmount")
+    @ResponseBody
+    public void setOrdtAmount(HttpSession session, @RequestParam int pId, @RequestParam int amount) {
+        @SuppressWarnings("unchecked")
+        List<OrderDetail> sCart = (List<OrderDetail>) session.getAttribute("cart");
+        List<OrderDetail> cart = orderService.setOrdtAmount(sCart, pId, amount);
+        session.setAttribute("cart", cart);
+    }
 }
